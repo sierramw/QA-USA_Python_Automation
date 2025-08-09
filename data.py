83cb2e57a5268680b457ec42375d6af9182cfb2d
SierraWilliams <swilliams9206@gmail.com>
2025-08-09 12:40:09 -0500

    commit

---
+++ data.py
@@ -0,0 +1,7 @@
+URBAN_ROUTES_URL = 'https://cnt-7c8c557f-75c3-4cfa-b3ce-930015078dea.containerhub.tripleten-services.com/'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
